efd66fd0d7b3cb64a5f2df597f01802602768ebf
Jora <nersisyanjor3@gamil.com>
2024-04-27 16:59:59 +0400

    Added uikit package

---
+++ packages/furniture_uikit/lib/src/widgets/buttons/furniture_elevated_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FurnitureElevatedButton extends StatelessWidget {
+  const FurnitureElevatedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: (){}, child: const Text('Elevated button'));
+  }
+}
