2db881a7b6bdfb88b27c684c08409eead686c1c9
AnnaMinasyan9 <nune.minasyan.1803@gmail.com>
2024-04-28 22:13:48 +0400

    Create Text Filed Styles
    Create Text Filed Styles

--- packages/furniture_uikit/lib/furniture_uikit.dart
+++ packages/furniture_uikit/lib/furniture_uikit.dart
@@ -2,13 +2,18 @@ library furniture_uikit;
 
 export 'src/widgets/buttons/furniture_elevated_icon_button.dart';
 export 'src/widgets/buttons/furniture_elevated_button.dart';
+
 export 'src/widgets/buttons/furniture_icon_button.dart';
 export 'src/widgets/buttons/furniture_text_button.dart';
 
 export 'src/widgets/furniture-cards/furniture_carousel_card.dart';
+
+export 'src/widgets/text_filed/furniture_text_filed_styles.dart';
 
 export 'src/assets/assets.gen.dart';
 export 'src/assets/fonts.gen.dart';
 export 'src/theme/furniture_main_theme.dart';
 
 export 'package:flutter_screenutil/flutter_screenutil.dart';
+
+export 'src/theme/furniture_colors.dart';
