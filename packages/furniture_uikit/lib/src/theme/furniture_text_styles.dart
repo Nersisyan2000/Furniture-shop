519746740c59324a1a56f6e334d2a6b25b309745
Jora <nersisyanjor3@gamil.com>
2024-04-30 19:36:18 +0400

    Rebase uikit/jora with me.

--- packages/furniture_uikit/lib/src/theme/furniture_text_styles.dart
+++ packages/furniture_uikit/lib/src/theme/furniture_text_styles.dart
@@ -1,7 +1,153 @@
 import 'package:flutter/material.dart';
 
+import 'furniture_colors.dart';
+
 String packageName = 'furniture_uikit';
 
 TextStyle get elevatedButtonTextStyle => TextStyle(
+      package: packageName,
+    );
+
+TextStyle get switzer24w5TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w500,
+      height: 32.0 / 24.0,
+      fontSize: 24.0,
+      color: FurnitureColors.textColor,
+      package: packageName,
+    );
+
+TextStyle get switzer16w4TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w400,
+      height: 24.0 / 16.0,
+      fontSize: 16.0,
+      color: FurnitureColors.textColor,
+      package: packageName,
+    );
+
+TextStyle get switzer16w5TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w500,
+      height: 24.0 / 16.0,
+      fontSize: 16.0,
+      color: FurnitureColors.textColor,
+      package: packageName,
+    );
+
+TextStyle get switzer16w6TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w600,
+      height: 24.0 / 16.0,
+      fontSize: 16.0,
+      color: FurnitureColors.textColor,
+      package: packageName,
+    );
+
+TextStyle get switzer32w5TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w500,
+      height: 32.0 / 40.0,
+      fontSize: 32.0,
+      color: FurnitureColors.textColor,
+      package: packageName,
+    );
+
+TextStyle get switzer32w6TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w600,
+      height: 32.0 / 40.0,
+      fontSize: 32.0,
+      color: FurnitureColors.textColor,
+      package: packageName,
+    );
+
+TextStyle get switzer14w5TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w500,
+      height: 14.0 / 20.0,
+      fontSize: 14.0,
+      color: FurnitureColors.textColor,
+      package: packageName,
+    );
+
+TextStyle get switzer14w4TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w400,
+      height: 14.0 / 20.0,
+      fontSize: 14.0,
+      color: FurnitureColors.textColor,
+      package: packageName,
+    );
+
+TextStyle get switzer13w4TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w400,
+      height: 13.0 / 18.0,
+      fontSize: 13.0,
+      color: FurnitureColors.textColor,
+      package: packageName,
+    );
+
+TextStyle get switzer13w5TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w500,
+      height: 13.0 / 18.0,
+      fontSize: 13.0,
+      color: FurnitureColors.textColor,
+      package: packageName,
+    );
+
+TextStyle get switzer20w6TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w600,
+      height: 20.0 / 28.0,
+      fontSize: 20.0,
+      color: FurnitureColors.textColor,
+      package: packageName,
+    );
+
+TextStyle get switzer20w5TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w500,
+      height: 20.0 / 28.0,
+      fontSize: 20.0,
+      color: FurnitureColors.textColor,
+      package: packageName,
+    );
+
+TextStyle get switzer12w4TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w400,
+      height: 12.0 / 16.0,
+      fontSize: 12.0,
+      color: FurnitureColors.textColor,
+      package: packageName,
+    );
+
+TextStyle get switzer12w5TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w500,
+      height: 12.0 / 16.0,
+      fontSize: 12.0,
+      color: FurnitureColors.textColor,
+      package: packageName,
+    );
+
+TextStyle get switzer11w5TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w500,
+      height: 11.0 / 14.0,
+      fontSize: 11.0,
+      color: FurnitureColors.textColor,
+      package: packageName,
+    );
+
+TextStyle get switzer11w4TextStyle => TextStyle(
+      fontFamily: "Switzer",
+      fontWeight: FontWeight.w400,
+      height: 11.0 / 14.0,
+      fontSize: 11.0,
+      color: FurnitureColors.textColor,
       package: packageName,
     );
