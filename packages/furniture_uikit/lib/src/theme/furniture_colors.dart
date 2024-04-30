94ae2d68d7a341cc4d4171a55fcd36624d51e090
Jora <nersisyanjor3@gamil.com>
2024-04-30 19:36:18 +0400

    Rebase uikit/jora with me.

---
+++ packages/furniture_uikit/lib/src/theme/furniture_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class FurnitureColors {
+  static const primaryColor = Color(0xFF0C8A7B);
+  static const textColor = Color(0xFF101817);
+  static const subTextColor = Color(0xFF828A89);
+  static const buttonShapeColor = Color(0xFFF0F0F2);
+  static const whiteColor = Color(0xFFFFFFFF);
+}
