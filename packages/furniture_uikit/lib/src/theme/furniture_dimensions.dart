33bdcd25549b6266484bd25dd1a3bb890c7ee200
Jora <nersisyanjor3@gamil.com>
2024-05-18 15:23:04 +0400

    Added some logic from screen util.

--- packages/furniture_uikit/lib/src/theme/furniture_dimensions.dart
+++ packages/furniture_uikit/lib/src/theme/furniture_dimensions.dart
@@ -9,43 +9,43 @@ EdgeInsets get paddingV16 => const EdgeInsets.symmetric(
       vertical: 16.0,
     );
 
-EdgeInsets get paddingV8 => const EdgeInsets.symmetric(vertical: 8.0);
+EdgeInsets get paddingV8 => EdgeInsets.symmetric(vertical: 8.h);
 
-EdgeInsets get paddingH16 => const EdgeInsets.symmetric(horizontal: 16.0);
+EdgeInsets get paddingH16 => EdgeInsets.symmetric(horizontal: 16.w);
 
 EdgeInsets get paddingAll16 => const EdgeInsets.all(16.0);
 
-EdgeInsets get paddingBottm8 => const EdgeInsets.only(bottom: 8.0);
+EdgeInsets get paddingBottm8 => EdgeInsets.only(bottom: 8.h);
 
 EdgeInsets get paddingAll7 => const EdgeInsets.all(7.0);
 
-EdgeInsets get paddingL16R8 => const EdgeInsets.only(left: 16.0, right: 8.0);
+EdgeInsets get paddingL16R8 => EdgeInsets.only(left: 16.w, right: 8.w);
 
-EdgeInsets get paddingH4 => const EdgeInsets.symmetric(horizontal: 4.0);
+EdgeInsets get paddingH4 => EdgeInsets.symmetric(horizontal: 4.w);
 
 EdgeInsets get paddingH12V8 =>
-    const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0);
+    EdgeInsets.symmetric(horizontal: 12.w, vertical: 8.h);
 
-EdgeInsets get paddingH8 => const EdgeInsets.symmetric(horizontal: 8.0);
+EdgeInsets get paddingH8 => EdgeInsets.symmetric(horizontal: 8.w);
 
 // Radiuses -------------------------------------------------------
 
 RoundedRectangleBorder get radius14 => RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(14.0),
+      borderRadius: BorderRadius.circular(14.r),
     );
 
 RoundedRectangleBorder get radius12 => RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(12.0),
+      borderRadius: BorderRadius.circular(12.r),
     );
 
 OutlineInputBorder get outlinedRadisus12 => OutlineInputBorder(
       borderSide: BorderSide.none,
-      borderRadius: BorderRadius.circular(12.0),
+      borderRadius: BorderRadius.circular(12.r),
     );
 
-BorderRadius get radius10Circular => BorderRadius.circular(10.0);
+BorderRadius get radius10Circular => BorderRadius.circular(10.r);
 
-Radius get radiusCircular4 => const Radius.circular(4.0);
+Radius get radiusCircular4 => Radius.circular(4.r);
 
 // BoxConstraints -------------------------------------------------
 
