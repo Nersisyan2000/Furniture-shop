29a9e33668702b38e794a70a6661ae9f49fe8cc0
Jora Nersisyan <nersisyanjor3@gmail.com>
2024-06-06 21:16:10 +0400

    Added fully provider.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:furniture_shop/presentation/screens/onboarding_screen/onboarding_screen_provider.dart';
 import 'package:furniture_shop/routes/app_router.dart';
 import 'package:furniture_uikit/furniture_uikit.dart';
 import 'package:furniture_localization/furniture_localization.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,17 +26,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(360, 812),
-      minTextAdapt: true,
-      child: MaterialApp.router(
-        localizationsDelegates: context.localizationDelegates,
-        supportedLocales: context.supportedLocales,
-        locale: context.locale,
-        title: 'Furniture Shop',
-        debugShowCheckedModeBanner: false,
-        theme: lightTheme,
-        routerConfig: _appRouter.config(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => OnBoardingProvider(),
+        ),
+      ],
+      child: ScreenUtilInit(
+        designSize: const Size(360, 812),
+        minTextAdapt: true,
+        child: MaterialApp.router(
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          title: 'Furniture Shop',
+          debugShowCheckedModeBanner: false,
+          theme: lightTheme,
+          routerConfig: _appRouter.config(),
+        ),
       ),
     );
   }
